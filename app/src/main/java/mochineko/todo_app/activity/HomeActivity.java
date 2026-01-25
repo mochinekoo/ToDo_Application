9fee5cfb82e21847004b131be33e0ca903341b9a
mochineko <mochinekoo0113@gmail.com>
2026-01-25 16:15:02 +0900

    [add] タスクのRecyclerViewを仮追加しました

--- app/src/main/java/mochineko/todo_app/activity/HomeActivity.java
+++ app/src/main/java/mochineko/todo_app/activity/HomeActivity.java
@@ -1,14 +1,22 @@
 package mochineko.todo_app.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import mochineko.todo_app.R;
+import mochineko.todo_app.json.TaskData;
+import mochineko.todo_app.recyclerview.TaskListRecyclerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,5 +30,15 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initialize();
+    }
+
+    public void initialize() {
+        RecyclerView task_recycler = findViewById(R.id.taskList_recycler);
+        TaskListRecyclerAdapter adapter = new TaskListRecyclerAdapter(this, Arrays.asList(new TaskData(0, "ああ", "aa", "RUNNING", "2025/10/10 12:10")));
+        task_recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        task_recycler.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/mochineko/todo_app/recyclerview/TaskListRecyclerAdapter.java
@@ -0,0 +1,58 @@
+package mochineko.todo_app.recyclerview;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import mochineko.todo_app.R;
+import mochineko.todo_app.json.TaskData;
+
+public class TaskListRecyclerAdapter extends RecyclerView.Adapter<TaskListRecyclerAdapter.ViewHolder> {
+
+    private Activity activity;
+    private List<TaskData> taskDataList;
+
+    public TaskListRecyclerAdapter(Activity activity, List<TaskData> taskDataList) {
+        this.activity = activity;
+        this.taskDataList = taskDataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.tasklist_layout, parent, false);
+        return new ViewHolder(view, activity);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TaskData taskData = taskDataList.get(position);
+        holder.taskName.setText(taskData.getName());
+        holder.taskDescription.setText(taskData.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskDataList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private Activity activity;
+        private TextView taskName, taskDescription;
+
+        public ViewHolder(@NonNull View itemView, Activity activity) {
+            super(itemView);
+            taskName = itemView.findViewById(R.id.taskName_text);
+            taskDescription = itemView.findViewById(R.id.task_description);
+            this.activity = activity;
+        }
+    }
+
+}
